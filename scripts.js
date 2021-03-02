93ef53bd69972361c59102e69542eafb3c96d937
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-02 03:42:47 +0100

    Carousel
    ccarousel

--- scripts.js
+++ scripts.js
@@ -33,7 +33,33 @@ $(document ).ready(function() {
             $("#smileschoolcarousel").show()    
         displayCarousels(data)})
       }
+
       fetchPosts()
+
+      $('#recipeCarousel').carousel({
+        interval: 10000
+      })
+      
+      $('.videocarousel .carousel-item').each(function(){
+          var minPerSlide = 3;
+          var next = $(this).next();
+          if (!next.length) {
+          next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+          
+          for (var i=0;i<minPerSlide;i++) {
+              next=next.next();
+              if (!next.length) {
+                  next = $(this).siblings(':first');
+                }
+              
+              next.children(':first-child').clone().appendTo($(this));
+            }
+      });
+      
+      
+      
 });
 
      
